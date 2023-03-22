789c0ec4232513c13f0de68909f612066d33fc5d
Esma Yılmaz <elektroyazilim@gmail.com>
2023-03-22 16:57:21 +0300

    foreach dongusu basic ex is added

---
+++ src/main/java/elektroyazilim/ForEachDongusu.java
@@ -0,0 +1,20 @@
+package elektroyazilim;
+
+public class ForEachDongusu {
+    public static void main(String[] args) {
+        String[] harfler = {"A", "B", "C", "D", "E"};
+
+        for (String harf: harfler) {
+            System.out.println(harf);
+        }
+
+        int[] sayilar = {1,2,3,6,8,3,5};
+
+
+        for(int sayi: sayilar)
+        {
+            System.out.println(sayi);
+        }
+
+    }
+}
